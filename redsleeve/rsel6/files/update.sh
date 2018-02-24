643d9c80772696db5e9090381729f3d7b3dc12da
ryan <rwoodsmall@gmail.com>
2018-02-24 02:56:35 +0000

    fix && instead of || for redsleeve-release check

--- redsleeve/rsel6/files/update.sh
+++ redsleeve/rsel6/files/update.sh
@@ -2,7 +2,7 @@
 
 rpm -q redsleeve-release \
 | grep -q 20170804 \
-  && rpm -Uvh http://ftp.redsleeve.org/pub/el6/latest/base/base/arm/Packages/redsleeve-release-6-20170804.el6.armv5tel.rpm
+  || rpm -Uvh http://ftp.redsleeve.org/pub/el6/latest/base/base/arm/Packages/redsleeve-release-6-20170804.el6.armv5tel.rpm
 sed -i '/^enabled=0/ s/^\(enabled\)=0/\1=1/g' /etc/yum.repos.d/update.repo
 yum clean all
 yum list updates

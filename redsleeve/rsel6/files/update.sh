39db8aae0426598e4db7a554f9ecf2e5d8a9c88c
ryan <rwoodsmall@gmail.com>
2018-02-24 03:02:48 +0000

    use redsleeve-release date and compare numerically before installing new release rpm

--- redsleeve/rsel6/files/update.sh
+++ redsleeve/rsel6/files/update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-rpm -q redsleeve-release \
-| grep -q 20170804 \
-  || rpm -Uvh http://ftp.redsleeve.org/pub/el6/latest/base/base/arm/Packages/redsleeve-release-6-20170804.el6.armv5tel.rpm
+rrd="$(rpm -q redsleeve-release --qf '%{RELEASE}\n' | cut -f1 -d.)"
+
+if [ ${rrd} -lt 20170804 ] ; then
+  rpm -Uvh http://ftp.redsleeve.org/pub/el6/latest/base/base/arm/Packages/redsleeve-release-6-20170804.el6.armv5tel.rpm
+fi
 sed -i '/^enabled=0/ s/^\(enabled\)=0/\1=1/g' /etc/yum.repos.d/update.repo
 yum clean all
 yum list updates

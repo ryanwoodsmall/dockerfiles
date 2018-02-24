b6392c9875bf9dd196cc537bdd172dce57e44ded
ryan <rwoodsmall@gmail.com>
2018-02-24 02:01:02 +0000

    update.sh script for new rpm mirror/gpg/etc.

---
+++ redsleeve/rsel6/files/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rpm -q redsleeve-release \
+| grep -q 20170804 \
+  && rpm -Uvh http://ftp.redsleeve.org/pub/el6/latest/base/base/arm/Packages/redsleeve-release-6-20170804.el6.armv5tel.rpm
+sed -i '/^enabled=0/ s/^\(enabled\)=0/\1=1/g' /etc/yum.repos.d/update.repo
+yum clean all
+yum list updates
+yum -y update

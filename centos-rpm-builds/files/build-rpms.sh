d03e0a647398892a23c64a50696745828ebbda56
ryan woodsmall <rwoodsmall@gmail.com>
2018-11-29 08:00:40 +0000

    centos-rpm-builds: i686 valgrind workaround for broken jq rpmbuild; comments, todo, cleanup

--- centos-rpm-builds/files/build-rpms.sh
+++ centos-rpm-builds/files/build-rpms.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# XXX - ~/rpmbuild should be set from "rpm --eval '%{_topdir}'"
+
+# exit on failure and be verbose
 set -eux
 
+# will be used for musl installation
 rpmarch="$(rpm --eval '%{_host}' | cut -f1 -d-)"
 
+# update everything, install some prereqs
 yum -y install deltarpm
 yum clean all
 yum list updates
@@ -33,6 +38,7 @@ yum -y install \
   readline-devel \
   valgrind
 
+# don't build debuginfo, set manual -static stuff on arm
 cat >~/.rpmmacros<<EOF
 %debug_package %{nil}
 EOF
@@ -43,6 +49,7 @@ fi
 mkdir -p ~/rpmbuild/SPECS
 mkdir -p ~/rpmbuild/SOURCES
 
+# check a bunch of repos out in ~/git
 mkdir -p ~/git
 cd ~/git
 for g in \
@@ -60,35 +67,41 @@ do
   git clone ${g}
 done
 
-for s in $(find ${PWD}/ -name \*.spec)
-do
+# add .spec symbolic links
+for s in $(find ${PWD}/ -name \*.spec) ; do
   echo ${s}
   ln -sf ${s} ~/rpmbuild/SPECS/
 done
 
+# download all sources
 rm -f ~/rpmbuild/SOURCES/*
 for s in ~/rpmbuild/SPECS/*.spec ; do
   echo ${s}
   spectool -g -A -C ~/rpmbuild/SOURCES/ ${s}
 done
 
+# build, install and bring musl into environment
 rm -f ~/rpmbuild/RPMS/${rpmarch}/musl*.rpm
 time rpmbuild -ba --clean ~/rpmbuild/SPECS/musl-static.spec
 rpm -Uvh ~/rpmbuild/RPMS/${rpmarch}/musl-static*.${rpmarch}.rpm
 source /etc/profile.d/musl-static.sh
 rm -f ~/rpmbuild/SPECS/musl-static.spec
 
-for s in ~/rpmbuild/SPECS/*{toybox,busybox}*.spec
-do
+# valgrind breaks on i686 for jq?
+if $(uname -m | grep -q '^i.*86$') ; then
+  sed -i '/^make check/s/make check/echo make check/g' ~/rpmbuild/SPECS/jq.spec
+fi
+
+# needed for clean compilation on arm
+for s in ~/rpmbuild/SPECS/*{toybox,busybox}*.spec ; do
   sed -i 's/HOSTCC=musl-gcc/HOSTCC="musl-gcc -static"/g' ${s}
 done
-for s in ~/rpmbuild/SOURCES/*_config_script.sh
-do
+for s in ~/rpmbuild/SOURCES/*_config_script.sh ; do
   sed -i '/^make/s/$/ HOSTCC="musl-gcc -static"/g' ${s}
 done
 
-for s in ~/rpmbuild/SPECS/*.spec
-do
+# build everything
+for s in ~/rpmbuild/SPECS/*.spec ; do
   echo ${s}
   time rpmbuild -ba --clean ${s}
 done

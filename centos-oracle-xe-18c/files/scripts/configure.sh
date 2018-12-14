98d40e29b1c8a22958c3ff84d9e069a8949bd84a
ryanwoodsmall <rwoodsmall@gmail.com>
2018-12-14 20:13:07 +0000

    centos-oracle-xe-18c: force memory for sga/pga to <= 2GB

--- centos-oracle-xe-18c/files/scripts/configure.sh
+++ centos-oracle-xe-18c/files/scripts/configure.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 cof="/oracle-xe-18c_configure.out"
+oconf="/etc/sysconfig/oracle-xe-18c.conf"
+oinit="/etc/init.d/oracle-xe-18c"
 
 if [ -e "${cof}" ] ; then
   echo "/etc/init.d/oracle-xe-18c has already been run"
@@ -11,18 +13,25 @@ if [ -z "${orapass}" ] ; then
   orapass="oracle"
 fi
 
+# XXX - this doesn't work?
 if [ -z "${DBCA_TOTAL_MEMORY}" ] ; then
-  export DBCA_TOTAL_MEMORY="4096"
+  export DBCA_TOTAL_MEMORY="2047"
 fi
 
 source /sethostname.sh
 
-sed -i.ORIG 's/SKIP_VALIDATIONS=false/SKIP_VALIDATIONS=true/g' /etc/sysconfig/oracle-xe-18c.conf
+sed -i.ORIG 's/SKIP_VALIDATIONS=false/SKIP_VALIDATIONS=true/g' "${oconf}"
+
+# set maximum memory to 1/2 of total to force sga/pga setting
+maxmem="$((($(free -g | awk '/^Mem:/{print $2}')/2)*(1024*1024)))"
+sed -i.ORIG "/^MAXIMUM_MEMORY=/s/^MAXIMUM_MEMORY=.*/MAXIMUM_MEMORY=${maxmem}/g" "${oinit}"
+# set pga/sga combined mem to 2GB-1MB to force split
+sed -i 's/memory=.*/memory=2047/g' "${oinit}"
 
 { echo "${orapass}" ; echo "${orapass}" ; } \
-| /etc/init.d/oracle-xe-18c configure 2>&1 \
+| "${oinit}" configure 2>&1 \
 | tee "${cof}"
 
-/etc/init.d/oracle-xe-18c stop
+"${oinit}" stop
 
 touch "${cof}"

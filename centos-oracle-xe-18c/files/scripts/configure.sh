5d089a90d8b9a131ac79108f5e20b17b5837f743
ryanwoodsmall <rwoodsmall@gmail.com>
2018-12-14 21:56:23 +0000

    centos-oracle-xe-18c: transparent hugepages? stumped

--- centos-oracle-xe-18c/files/scripts/configure.sh
+++ centos-oracle-xe-18c/files/scripts/configure.sh
@@ -18,14 +18,22 @@ if [ -z "${DBCA_TOTAL_MEMORY}" ] ; then
   export DBCA_TOTAL_MEMORY="2048"
 fi
 
+if ! $(grep -q '\[never\]' /sys/kernel/mm/transparent_hugepage/enabled) ; then
+  echo "transparent hugepages may cause issues on xe"
+  echo "if configuration fails, try disabling"
+fi
+
 source /sethostname.sh
 
 sed -i.ORIG 's/SKIP_VALIDATIONS=false/SKIP_VALIDATIONS=true/g' "${oconf}"
 
+sed -i.ORIG '/"sga_target"/a <initParam name="sga_max_size" value="1536" unit="MB"/>' /opt/oracle/product/18c/dbhomeXE/assistants/dbca/templates/XE_Database.dbc
+sed -i '/"sga_target"/a <initParam name="use_large_pages" value="FALSE"/>' /opt/oracle/product/18c/dbhomeXE/assistants/dbca/templates/XE_Database.dbc
+
 # set maximum memory to 1/2 of total to force sga/pga setting
 maxmem="$((($(free -g | awk '/^Mem:/{print $2}')/2)*(1024*1024)))"
 sed -i.ORIG "/^MAXIMUM_MEMORY=/s/^MAXIMUM_MEMORY=.*/MAXIMUM_MEMORY=${maxmem}/g" "${oinit}"
-# set pga/sga combined mem to 2GB-1MB to force split
+# set pga/sga combined mem to 2GB to force split
 sed -i 's/memory=.*/memory=2048/g' "${oinit}"
 
 { echo "${orapass}" ; echo "${orapass}" ; } \

5390cd52dfd949e2221811287f07dbd5d459b045
ryanwoodsmall <rwoodsmall@gmail.com>
2019-01-28 07:04:36 +0000

    crosware: add statictoolchain builder

---
+++ crosware/statictoolchain/files/build-statictoolchain.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -eu
+source /usr/local/crosware/etc/profile
+crosware install make screen git binutils
+source /usr/local/crosware/etc/profile
+cd
+git clone https://github.com/richfelker/musl-cross-make.git
+cd musl-cross-make
+curl -kLO https://raw.githubusercontent.com/ryanwoodsmall/musl-misc/master/musl-cross-make-confs/Makefile.arch_indep
+( date ; time ( make -f Makefile.arch_indep ; echo $? ) ; date ) >/tmp/musl-cross-make.out 2>&1

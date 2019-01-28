697f4e5f3b7776a8c7fb8c3547f1b575fa3e7f4e
ryanwoodsmall <rwoodsmall@gmail.com>
2019-01-28 07:41:36 +0000

    crosware: for statictoolchain, use a logfile var, dump talky output to it

--- crosware/statictoolchain/files/build-statictoolchain.sh
+++ crosware/statictoolchain/files/build-statictoolchain.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
+
 set -eu
+
+logfile="/tmp/musl-cross-make.out"
+echo "logging to ${logfile}"
+
 source /etc/profile
 source /usr/local/crosware/etc/profile
-crosware install git binutils
+
+echo "installing prerequisites"
+crosware install git binutils >${logfile} 2>&1
 source /usr/local/crosware/etc/profile
 cd
+
+echo "cloning musl-cross-make"
 export GIT_SSL_NO_VERIFY=1
-git clone https://github.com/richfelker/musl-cross-make.git
+git clone https://github.com/richfelker/musl-cross-make.git >>${logfile} 2>&1
+
 cd musl-cross-make
+
+echo "getting Makefile.arch_indep"
 curl -kLO https://raw.githubusercontent.com/ryanwoodsmall/musl-misc/master/musl-cross-make-confs/Makefile.arch_indep
-( date ; time ( make -f Makefile.arch_indep ; echo $? ) ; date ) >/tmp/musl-cross-make.out 2>&1
+
+echo "building compiler"
+( date ; time ( make -f Makefile.arch_indep ; echo $? ) ; date ) >>${logfile} 2>&1

fb85dff030470a7e8a83e412c1f3350b42afe806
ryan woodsmall <rwoodsmall@gmail.com>
2019-03-19 20:34:04 +0000

    crosware: add ccache container volume/builder script

---
+++ crosware/ccache/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+
+basecon="ryanwoodsmall/crosware:latest"
+ccachecon="crosware-ccache"
+crosware="/usr/local/crosware/bin/crosware"
+
+declare -A volumes
+volumes["crosware-ccache"]="/root/.ccache"
+volumes["crosware-downloads"]="/usr/local/crosware/downloads"
+vopts=""
+
+for v in ${!volumes[@]} ; do
+  docker volume ls --quiet --filter name="${v}" | wc -l | grep -q '^1$' \
+  || docker volume create "${v}"
+  vopts+="-v ${v}:${volumes[${v}]} "
+done
+
+docker rm ${ccachecon} || true
+docker run --name ${ccachecon} ${vopts} ${basecon} bash -l ${crosware} install ccache make \
+&& docker commit -c 'CMD ["bash","-il"]' ${ccachecon} ${basecon}  \
+&& docker rm ${ccachecon}

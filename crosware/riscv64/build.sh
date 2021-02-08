314071c82448713e4dcdd6ec280083b43238be7d
ryanwoodsmall <rwoodsmall@gmail.com>
2021-02-07 22:30:44 -0600

    crosware/riscv64/build.sh: use variables

--- crosware/riscv64/build.sh
+++ crosware/riscv64/build.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
-( time ( docker image pull ryanwoodsmall/crosware
-         curl -kLs https://github.com/ryanwoodsmall/dockerfiles/raw/master/crosware/riscv64/Dockerfile \
-         | docker build --tag blah - \
-           && docker run --name blah blah bash -c 'uname -m ; curl --version ; bash --version ; set | egrep "(TYPE|BASH)"' \
-           && docker export blah \
-              | docker import -c 'CMD ["bash","-il"]' -c 'WORKDIR /usr/local/crosware' - ryanwoodsmall/crosware:riscv64 \
-                && docker image push ryanwoodsmall/crosware:riscv64 \
-                && docker rm blah \
-                && docker image rm blah
-) ) 2>&1 | tee /tmp/riscv64build.out
+set -e
+set -u
+set -o pipefail
+
+v="ryanwoodsmall"
+i="crosware"
+a="riscv64"
+c="${i}build${a}"
+
+docker rm ${c} || true
+docker image rm ${c} || true
+docker image rm ${v}/${i}:${a} || true
+
+( time ( docker image pull ${v}/${i}
+         curl -kLs https://github.com/${v}/dockerfiles/raw/master/${i}/${a}/Dockerfile \
+         | docker build --tag ${c} - \
+           && docker run --name ${c} ${c} bash -c 'uname -m ; curl --version ; bash --version ; set | egrep "(TYPE|BASH)"' \
+           && docker export ${c} \
+              | docker import -c 'CMD ["bash","-il"]' -c 'WORKDIR /usr/local/crosware' - ${v}/${i}:${a} \
+                && docker image push ${v}/${i}:${a} \
+                && docker rm ${c} \
+                && docker image rm ${c}
+) ) 2>&1 | tee /tmp/${a}build.out
